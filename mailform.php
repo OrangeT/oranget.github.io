4667bc8572a3e7449ff8cabe4a48cdd3a7016a20
Kian Ryan <kian@orangetentacle.co.uk>
2014-04-13 15:05:39 +0100

    Added thanks mailform

---
+++ mailform.php
@@ -0,0 +1,16 @@
+<?php
+  $email = $_HEADER['email'];
+  $name = $_HEADER['name'];
+  $phonenumber = $_HEADER['phonenumber'];
+  $message = $_HEADER['message'];
+
+  $body = "Email: " + $email + "\n"
+	  + "Name: " + $name + "\n"
+	  + "Phone Number: " + $phonenumber + "\n"
+	  + "Message: \n" + $message;
+
+  mail( "hello@orangetentacle.co.uk", "Website Contact",
+    $body, "From: $email" );
+
+  header( "Location: http://www.orangetentacle.co.uk/thanks.html" );
+?>
