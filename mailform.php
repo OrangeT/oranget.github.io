acad7c826b1a9cc9565df3c03f3c2f09cd735915
Kian Ryan <kian@orangetentacle.co.uk>
2014-06-25 14:25:18 +0100

    Anti-robot protection

--- mailform.php
+++ mailform.php
@@ -3,14 +3,20 @@
   $name = $_POST['name'];
   $phonenumber = $_POST['phonenumber'];
   $message = $_POST['message'];
+  $robot = $_POST['url'];
 
-  $body = "Email: " . $email . "\n"
-	  . "Name: " . $name . "\n"
-	  . "Phone Number: " . $phonenumber . "\n"
-	  . "Message: \n" . $message;
+  if (strtolower($robot) == 'no')
+  {
+    $body = "Email: " . $email . "\n"
+            . "Name: " . $name . "\n"
+            . "Phone Number: " . $phonenumber . "\n"
+            . "Message: \n" . $message;
 
-  mail( "hello@orangetentacle.co.uk", "Website Contact",
-    $body, "From: hello@orangetentacle.co.uk" );
+    mail( "hello@orangetentacle.co.uk", "Website Contact",
+            $body, "From: hello@orangetentacle.co.uk" );
 
-  header( "Location: http://www.orangetentacle.co.uk/thanks.html" );
+    header( "Location: http://www.orangetentacle.co.uk/thanks.html" );
+  } else {
+    header( "Location: http://www.orangetentacle.co.uk/robot.html" );
+  }
 ?>
